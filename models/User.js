1aab8febf08c28b19a96a3d77f67c22d3af2b836
leo-mutuku <le.mutuku@gmail.com>
2023-05-28 22:09:03 +0300

    fix conflit on model/user

--- models/User.js
+++ models/User.js
@@ -1,36 +1,6 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-<<<<<<< HEAD
-        name: {
-            type: String, 
-            required: [true, 'Please provide name'],
-            maxlength: 50,
-            minlength: 5,
-        }, 
-        email: {
-            type: String,
-            required: [true, 'Please provide email'],
-            match: [
-              /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-              'Please provide a valid email',
-            ],
-            unique: true,
-          },
-          password:{
-            type: String, 
-            required: [true, 'Please provide password'],
-            minlength: 6,
-          },
-          phoneNumber: {
-            type: String, 
-            minlength:10,
-            maxlength:10,
-          },
-          streetAddress: {
-            type: String, 
-            required: [true, 'Please provide street address'],
-=======
 	name: {
 		type: String,
 		required: [true, 'Please provide name'],
@@ -73,6 +43,5 @@ const UserSchema = new mongoose.Schema({
 		enum: ['buyer', 'seller'],
 	},
 })
->>>>>>> dev
 
 module.exports = mongoose.model('User', UserSchema)
