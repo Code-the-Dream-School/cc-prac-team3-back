a7bb95c7ce8ab45b7b26feeaff5e482a94ed3e90
Kenya <pascasciokenya97@gmail.com>
2023-06-13 16:18:29 -0400

    create product was declared twice so I deleted one of the declarations

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -48,12 +48,6 @@ const getProductsBySearch = async (req, res) => {
 	}
 }
 
-const createProduct = async (req, res)=>{
-    req.body.createdBy = req.user.userId
-    const product = await Product.create(req.body)
-    res.status(StatusCodes.CREATED).json({ product })
-}
-
 const getProductsByFilter = async (req, res) =>{
     //going to use query params here I think. 
 }
