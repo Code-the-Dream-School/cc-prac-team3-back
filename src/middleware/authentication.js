81545b6352c8cb9c77a98a4d771c5a71b4141be8
Kenya <pascasciokenya97@gmail.com>
2023-06-25 08:24:08 -0400

    modified product model to include productImage property and tested in Postman

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -6,7 +6,6 @@ const authMiddleware = async (req, res, next)=>{
     //check header
     const authHeader = req.headers.authorization
     if(!authHeader || !authHeader.startsWith('Bearer')){
-        console.log(error)
         throw new UnauthenticatedError('Authentication invalid') 
     }
     const token = authHeader.split(' ')[1]
@@ -14,20 +13,12 @@ const authMiddleware = async (req, res, next)=>{
     try{
         const payload = jwt.verify(token, process.env.JWT_SECRET)
         req.user = { userId: payload.userId, name: payload.name }
-        next() //deleted semicolon
+        next()//deleted semicolon
     } catch(error){
         console.log(error)
         throw new UnauthenticatedError ('authentication invalid')
 
     }
-
-	try {
-		const payload = jwt.verify(token, process.env.JWT_SECRET)
-		req.user = { userId: payload.userId, name: payload.name }
-		next()
-	} catch (error) {
-		throw new UnauthenticatedError('authentication invalid')
-	}
 }
 
 module.exports = authMiddleware
